414e31aed4791e7e4f3ccc49d5f43bcb05990374
Alberto <albertoyanesjr3@hotmail.com>
2019-05-26 09:49:53 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './components/Counter'
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Counter />
+  );
+}
+
+export default App;
